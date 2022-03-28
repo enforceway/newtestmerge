b2a9b22f1a0f30444e593c06662f7dfa06730a1d
lizhaolongamd <lizhaolongamd@163.com>
2022-03-28 22:51:40 +0800

    update default.js

--- src/default.js
+++ src/default.js
@@ -1,1 +1,7 @@
-console.log('default.js')
+function formate(dateObj, formateStr) {
+	if (!formateStr) {
+		formateStr = "yyyy-MM-dd"
+	}
+	var result = formateStr.replace('yyyy', dateObj.getFullYear()).replace('MM', dateObj.getMonth() + 1).replace('dd', dateObj.getDate());
+	return result;
+}
